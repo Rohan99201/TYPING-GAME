16934a13a2377720a7fc7616e833c8fd9ba21ebb
Rohan04 <rohanchoubey04@gmail.com>
2021-03-16 21:07:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,116 @@
+"use-strict";
+
+// Grabbing html elements
+//Buttons
+const startButton = document.getElementById("start-button");
+const retryButton = document.getElementById("retry-button");
+// Typing content
+const textDiv = document.getElementById("text");
+// Sounds
+const clickSound = new Audio("sounds/buttonClick.mp3");
+// Stats elements
+const statsDiv = document.getElementById("stats");
+const time = document.getElementById("time-elapsed");
+const wordsPerMin = document.getElementById("wpm");
+const accuracy = document.getElementById("accuracy");
+const correctInput = document.getElementById("correctInput");
+const incorrectInput = document.getElementById("incorrectInput");
+
+// Hiding elements before game start
+textDiv.classList.add("hide");
+statsDiv.classList.add("hide");
+retryButton.classList.add("hide");
+
+// Wrapping everything in a start button listener
+startButton.addEventListener("click", function () {
+  clickSound.play();
+
+  // Displaying elements, hiding start button
+  textDiv.classList.remove("hide");
+  retryButton.classList.remove("hide");
+  startButton.classList.add("hide");
+
+  // Spliting text into characters
+  let randomPara = textLib[Math.trunc(Math.random() * textLib.length)];
+  const textIntoCharacters = randomPara.split("");
+
+  const characters = textIntoCharacters.map((char) => {
+    const span = document.createElement("span");
+    span.innerText = char;
+    textDiv.appendChild(span);
+    return span;
+  });
+
+  // Typing - Starting positition
+  let cursorPosition = 0;
+  let cursorCharacter = characters[cursorPosition];
+  cursorCharacter.classList.add("cursor");
+
+  // Tracking right/wrong input just for statistics
+  let wrongLetters = 0;
+  let correctLetters = 0;
+
+  // Setting initial time to null
+  let startTime = null;
+  let endTime = null;
+
+  // Listening for button press, starting the time
+  const keypress = ({ key }) => {
+    // Time only starts when user starts typing
+    if (!startTime) {
+      startTime = new Date();
+    }
+
+    if (key === cursorCharacter.innerText) {
+      cursorCharacter.classList.remove("cursor");
+      cursorCharacter.classList.remove("wrong");
+      cursorCharacter.classList.add("done");
+      cursorCharacter = characters[++cursorPosition];
+      correctLetters++;
+    } else {
+      cursorCharacter.classList.add("wrong");
+      cursorCharacter.classList.remove("done");
+      cursorCharacter.classList.remove("cursor");
+      cursorCharacter = characters[--cursorPosition];
+      wrongLetters++;
+    }
+
+    // This is how I fixed cursor value going into negatives which bricks the game (if you type wrong first character)
+    if (cursorPosition < 0) {
+      cursorCharacter = characters[0];
+      cursorCharacter = characters[++cursorPosition];
+    }
+
+    // Calculating stats, ending time
+    if (cursorPosition >= characters.length) {
+      endTime = new Date();
+      const timeElapsed = endTime - startTime;
+      const seconds = (timeElapsed / 1000).toFixed(1);
+      const wordNumber = randomPara.split(" ").length;
+      const wps = (wordNumber / seconds).toFixed(1);
+      const wpm = (wps * 60).toFixed(1);
+
+      // Removing key listening at game end
+      document.removeEventListener("keypress", keypress);
+
+      // Hide text when game is done
+      textDiv.classList.add("hide");
+
+      // Stats that are displayed at game end
+      correctLetters -= 2 * wrongLetters;
+      statsDiv.classList.remove("hide");
+
+      time.textContent = `${seconds}s`;
+      wordsPerMin.textContent = `${wpm}`;
+      accuracy.textContent = `${(
+        (correctLetters / characters.length) *
+        100
+      ).toFixed(1)}%`;
+      correctInput.textContent = ` ${correctLetters}`;
+      incorrectInput.textContent = `${wrongLetters}`;
+      return;
+    }
+    cursorCharacter.classList.add("cursor");
+  };
+  document.addEventListener("keypress", keypress);
+});
---
+++ words.js
@@ -0,0 +1,15 @@
+const textLib = [
+  `Zebras are several species of African equids (horse family) united by their distinctive black and white striped coats. Their stripes come in different patterns, unique to each individual. They are generally social animals that live in small harems to large herds.Unlike their closest relatives the horses and donkeys, zebras have never been truly domesticated.`,
+
+  `As you can see in the title this is "A very hard typing test text." Someone must have the worldwide record as at least 60 seconds to he put up on my typing test wall! I will be checking this page every day to see if someone got this score. Most likely I will! Me! It's very fun making these types of elaborate things because you can write whatever you want.`,
+
+  `Proofreader applicants are tested primarily on their spelling, speed, and skill in finding errors in the sample text. Toward that end, they may be given a list of ten or twenty classically difficult words and a proofreading test, both tightly timed. The proofreading test will often have a maximum number of errors per quantity of text and a minimum amount of time to find them.`,
+
+  `Web designers are expected to have an awareness of usability and if their role involves creating mark up then they are also expected to be up to date with web accessibility guidelines. The different areas of web design include web graphic design; interface design; authoring, including standardised code and proprietary software; user experience design; and search engine optimization.`,
+
+  `A typewriter is a mechanical or electromechanical machine for typing characters similar to those produced by a printer's movable type.`,
+
+  `"The quick brown fox jumps over the lazy dog" is an English-language pangrama sentence that contains all of the letters of the English alphabet. Owing to its brevity and coherence, it has become widely known. The phrase is commonly used for touch-typing practice, testing typewriters and computer keyboards, displaying examples of fonts, and other applications involving text where the use of all letters in the alphabet is desired.`,
+
+  `The quick brown fox jumps over the lazy dog The goal of this approach is to identify those with the best skill set.`,
+];
